23ec1cc906314bb2c2809ebdeaecbf3c05f675ee
makmemeng <yatani.mems4568@gmail.com>
2021-11-09 10:35:34 +0900

    条件分岐 v-if

--- HelloWorld/js/main.js
+++ HelloWorld/js/main.js
@@ -1,13 +1,6 @@
 const app = Vue.createApp({
   data: ()　=> ({
-    message: 'Hello Vue.js!',
-    count: 99,
-    user: {
-      lastName: 'Yatani',
-      firstName: 'Makio',
-      prefecture: 'Tokyo'
-    },
-    colors: ['Red', 'Green', 'Blue']
+    toggle: true
   })
 })
 app.mount('#app')
